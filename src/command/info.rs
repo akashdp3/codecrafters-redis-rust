e8b87f4c330f030cc55ac6fbc99f4f354c76c373
akashdp3 <akashdeep.samantra@protonmail.com>
2026-03-02 01:29:36 +0530

    XC1: Initial replication ID and offset

--- src/command/info.rs
+++ src/command/info.rs
@@ -23,14 +23,28 @@ pub(crate) fn parse(args: &mut impl Iterator<Item = String>) -> anyhow::Result<C
 
 pub(crate) fn invoke(store: &mut Store, kind: InfoKind) -> anyhow::Result<Resp> {
     let value = match kind {
-        InfoKind::Replication => {
-            match store.config.replica_of() {
-                "" => "role:master",
-                _replica_info => "role:slave"
-            }
-        }
-        InfoKind::All => "role:master"
+        InfoKind::Replication => replication_info(store.config.replica_of()),
+        // TODO: for InfoKind::All, need to parse all values
+        InfoKind::All => replication_info(store.config.replica_of())
     };
 
     Ok(Resp::bulk(value))
+}
+
+fn replication_info(replica_info: &str) -> String {
+    let mut result = vec![];
+
+    let role = match replica_info {
+        "" => "master",
+        _info => "slave"
+    };
+    result.push(format!("role:{role}"));
+
+    let master_replid = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+    result.push(format!("master_replid:{master_replid}"));
+
+    let master_repl_offset = 0;
+    result.push(format!("master_repl_offset:{master_repl_offset}"));
+
+    result.join("\r\n")
 }
