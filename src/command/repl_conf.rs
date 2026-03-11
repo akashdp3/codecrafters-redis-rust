8e2c993d61e9034a38d89552df71ded8b80dc81e
akashdp3 <email@akashdp.com>
2026-03-11 14:48:10 +0530

    XV6: ACK with no commands

--- src/command/repl_conf.rs
+++ src/command/repl_conf.rs
@@ -6,15 +6,19 @@ use anyhow::Context;
 pub(crate) enum Kind {
     ListeningPort,
     Capabilities,
+    GetAck,
+    Ack,
 }
 
 impl FromStr for Kind {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
+        match s.to_lowercase().as_str() {
             "listening-port" => Ok(Kind::ListeningPort),
             "capa" => Ok(Kind::Capabilities),
+            "getack" => Ok(Kind::GetAck),
+            "ack" => Ok(Kind::Ack),
             _ => Err(anyhow::anyhow!("Invalid REPLCONF key: {}", s)),
         }
     }
@@ -32,6 +36,15 @@ pub(crate) fn parse(args: &mut impl Iterator<Item = String>) -> anyhow::Result<C
     Ok(Command::ReplConf { key, value })
 }
 
-pub(crate) fn invoke(_store: &mut Store, _key: Kind, _value: &str) -> anyhow::Result<Resp> {
-    Ok(Resp::ok())
+pub(crate) fn invoke(_store: &mut Store, key: Kind, value: &str) -> anyhow::Result<Resp> {
+    let result = match key {
+        Kind::GetAck => Resp::array(vec![
+            "REPLCONF".to_string(),
+            "ACK".to_string(),
+            "0".to_string(),
+        ]),
+        _ => Resp::ok(),
+    };
+
+    Ok(result)
 }
